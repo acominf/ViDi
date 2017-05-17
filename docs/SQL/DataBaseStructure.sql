295444238b0c9639e97e990091320c54e01ba9d4
Fernando_lap <feral1592@gmail.com>
2017-05-17 09:28:51 -0500

    Estructura de la base de datos

--- docs/SQL/DataBaseStructure.sql
+++ docs/SQL/DataBaseStructure.sql
@@ -3,10 +3,39 @@ CREATE DATABASE ViDi;
 use vidi;
 
 CREATE Table users(
+	id int auto_increment,
     usr varchar(16) not null,
     pass varchar(16) not null,
     nombre varchar(20) NOT null,
     apellido varchar(30),
     edad int not null,
-    PRIMARY KEY (usr)
+    PRIMARY KEY (id),
+    UNIQUE nombre_usuario (usr)
+);
+
+create table video(
+	id int not null auto_increment,
+	nombre varchar(50) not null,
+	PRIMARY KEY (id),
+	UNIQUE nombre_usuario (nombre)
+);
+
+create table clasif(
+	id int not null auto_increment,
+	clas varchar(16) not null,
+	PRIMARY KEY (id)
+);
+
+create table video_clasif(
+	idVideo int not null,
+	idClas int not null,
+	PRIMARY KEY (idVideo,idClas)
+);
+
+create table user_video(
+	idVideo int not null,
+	idUser int not null
+	puntuacion int not null
+
+	CHECK (puntuacion>=0 && puntuacion<=5)
 );
